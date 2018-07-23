f3467cfd1e27560caae79660d9fb130d8114205b
Vitaliy <vitaliy@altsolution.net>
2018-07-23 09:29:20 +0300

    add morgan

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const cors = require ('cors')
+const morgan = require ('morgan')
 
 const apiRoutes = require('./routes/api')
 const app = express()
 
+app.use(morgan('dev'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors)
 
 app.use('/api', apiRoutes)
 
