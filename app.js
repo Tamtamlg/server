9eafce34c0c0c14837f88b2a3884c6af6ed1c242
Vitaliy <vitaliy@altsolution.net>
2018-07-24 15:43:24 +0300

    update json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const cors = require ('cors')
-const morgan = require ('morgan')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const apiRoutes = require('./routes/api')
 const app = express()
--- controllers/api.js
+++ controllers/api.js
@@ -3,16 +3,12 @@ const fs = require('fs');
 module.exports.login = function (req, res) {
   if (req.body.email && req.body.password) {
     res.status(200).json({
-      status: 'S',
-      data: {
         expired_at: '2018-07-21 16:09:50',
         role: 'admin',
         token: 'LOg1QOpZPzVOFZaL-qf6hKqXcwUZcjx5U-sAfindBHsUEweVyj-FGan3vOLCBxW0csi-MTUzMjAwOTc5MQ=='
-      }
     })
   } else {
-    res.status(200).json({
-      status: 'E',
+    res.status(401).json({
       message: 'Incorrect login or password'
     })
   }
@@ -24,14 +20,16 @@ module.exports.api = function (req, res) {
     if (Math.floor(Math.random() * 10) < 8) {
       console.log('level1-' + (Math.floor(Math.random() * (4 - 1)) + 1) + '.json')
       fs.readFile('level1-' + (Math.floor(Math.random() * (4 - 1)) + 1) + '.json', 'utf8', function (err, contents) {
+		const jsonContent = JSON.parse(contents)
         res.status(200).json({
-          contents
+          jsonContent
         });
       });
     } else {
       fs.readFile('error.json', 'utf8', function (err, contents) {
+		const jsonContent = JSON.parse(contents)
         res.status(200).json({
-          contents
+          jsonContent
         });
       });
     }
